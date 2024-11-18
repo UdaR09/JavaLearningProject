1d8da6fd3e397994db4618a1490907c9a7eeca1c
UdaR09 <radumost2001@gmail.com>
2024-11-18 11:37:05 +0200

    Added the Variables and Comments Task.

--- src/main/java/com/trimetrica/Main.java
+++ src/main/java/com/trimetrica/Main.java
@@ -6,12 +6,48 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //Declarare date primitive
+
+        boolean trueBool;
+        boolean falseBool;
+        char primulAscii;
+        char ultimulAscii;
+        byte celMaiMicByte;
+        byte celMaiMareByte;
+        short celMaiMicShort;
+        short celMaiMareShort;
+        int celMaiMicInt;
+        int celMaiMareInt;
+        long celMaiMicLong;
+        long celMaiMareLong;
+        float celMaiMicFloat;
+        float celMaiMareFloat;
+        double celMaiMicDouble;
+        double celMaiMareDouble;
+
+        trueBool = true;
+        falseBool = false;
+
+        primulAscii = 0;
+        ultimulAscii = 65535;
+
+        celMaiMicByte = -128;
+        celMaiMareByte = 127;
+
+        celMaiMicShort = -32768;
+        celMaiMareShort = 32767;
+
+        celMaiMicInt = -2147483648;
+        celMaiMareInt = 2147483647;
+
+        celMaiMicLong = Long.MIN_VALUE;
+        celMaiMareLong = Long.MAX_VALUE;
+
+        celMaiMicFloat = Float.MIN_VALUE;
+        celMaiMareFloat = Float.MAX_VALUE;
+
+        celMaiMicDouble = Double.MIN_VALUE;
+        celMaiMareDouble = Double.MAX_VALUE;
     }
 }
