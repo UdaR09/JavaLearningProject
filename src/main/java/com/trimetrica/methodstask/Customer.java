2f796333a47684bff90a281b8ab2e7fc5460e1f5
Radu <radumost2001@gmail.com>
2025-02-15 10:14:45 +0200

    Add 2 classes

---
+++ src/main/java/com/trimetrica/methodstask/Customer.java
@@ -0,0 +1,25 @@
+package com.trimetrica.methodstask;
+
+public class Customer {
+    private String name, email;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/main/java/com/trimetrica/methodstask/ExecutionService.java
@@ -0,0 +1,9 @@
+package com.trimetrica.methodstask;
+
+public class ExecutionService {
+    public static void main(String[] args) {
+    Customer customer001 = new Customer();
+    customer001.setName("Gabi");
+    System.out.println(customer001.getName());
+    }
+}
